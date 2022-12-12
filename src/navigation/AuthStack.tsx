e691bf439846aaf50e5137ff98d85070549477d8
27ashmita <ashmitaghimire1@gmail.com>
2022-12-12 22:48:19 +0545

    feat: implemetatyio on register view

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,7 @@ const AuthStack = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Login" component={LoginScreen} />
-      {/* // <Stack.Screen name="Register" component={RegisterScreen} /> */}
+      <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
